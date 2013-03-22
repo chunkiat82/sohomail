ff714e390ea28718e98a4cf1f18a2f20b63b8f08
Raymond Ho <chunkiat82@gmail.com>
2013-03-23 00:34:38 +0800

    progresss endpoint added

--- routes/queue.js
+++ routes/queue.js
@@ -1,9 +1,15 @@
 var models = require('../models');
 var _ = require("underscore");
+var passport = require('passport');
 var states = {"active":0, "inactive":-1, "completed":1};
 
-exports.list = function(req, res){
-  res.locals.session = req.session;
+function logger(data){
+  console.log("Queue : "+data);
+}
+
+exports.list =  [
+  passport.authenticate(['basic', 'oauth2-client-password', 'header'], { session: false }),
+  function(req, res){
   var queues = models.EmailQueue.find().select().exec(function(err, data){
     var body =  JSON.stringify(data,function censor(key, values) {
         if (key == "jobs") {
@@ -21,10 +27,11 @@ exports.list = function(req, res){
     res.setHeader('Content-Length', body.length);
     res.end(body);  
   });
-};
+}];
 
-exports.get = function(req, res){
-  res.locals.session = req.session;
+exports.get =  [
+  passport.authenticate(['basic', 'oauth2-client-password', 'header'], { session: false }),
+  function(req, res){
   if (req.param('queue') in states){
     console.log("status");
     status(req, res);
@@ -33,8 +40,31 @@ exports.get = function(req, res){
     console.log("get");
     get(req, res);
   } 
-};
+}];
 
+exports.progress =  [
+  passport.authenticate(['basic', 'oauth2-client-password', 'header'], { session: false }),
+  function(req, res){
+  var queueId = req.param('queue');
+
+  //total
+  models.EmailQueue.find({"queue":queueId}).count().exec(function (err,dataTotal){    
+    if (data) {
+      //sent total
+      models.EmailQueue.find({"queue":queueId, "status":"sent" }).count().exec(function (err,dataSent){          
+        return res.json(200, {progress: (dataSent/dataTotal)});
+      });
+    }
+    else{
+      return res.json(500, {});
+    }
+  });
+
+  
+
+}];
+
+//this is not right need to rewrite because it is should be loading status it should be raw request
 function status(req, res){
   var queues = models.EmailQueue.find({'status':req.param('queue')}).select().exec(function(err, data){
     var body =  jsonJobFormatter(data);
