a6b613261c28007f403932f3c83b68fdaedc2adc
Raymond Ho <chunkiat82@gmail.com>
2013-04-20 14:07:28 +0800

    report code test

--- routes/queue.js
+++ routes/queue.js
@@ -1,112 +1,185 @@
 var models = require('../models');
 var _ = require("underscore");
 var passport = require('passport');
-var states = {"active":0, "inactive":-1, "completed":1};
+var states = {
+  "active": 0,
+  "inactive": -1,
+  "completed": 1
+};
 
-function logger(data){
-  console.log("Queue : "+data);
+function logger(data) {
+  console.log("Queue : " + data);
 }
 
-exports.list =  [
-  passport.authenticate(['basic', 'oauth2-client-password', 'header'], { session: false }),
-  function(req, res){
-  var queues = models.EmailQueue.find().select().exec(function(err, data){
-    var body =  JSON.stringify(data,function censor(key, values) {
-        if (key == "jobs") {
-          var finalValues = [];
-          console.log(values);
-          _.each(values, function(value, key, list){
-          value = '<a href=\'/job/'+value+'\'>'+value+'</a>';
+exports.list = [
+passport.authenticate(['basic', 'oauth2-client-password', 'header'], {
+  session: false
+}),
+
+function(req, res) {
+  var queues = models.EmailQueue.find().select().exec(function(err, data) {
+    var body = JSON.stringify(data, function censor(key, values) {
+      if (key == "jobs") {
+        var finalValues = [];
+        console.log(values);
+        _.each(values, function(value, key, list) {
+          value = '<a href=\'/job/' + value + '\'>' + value + '</a>';
           finalValues.push(value);
         });
-          return finalValues;
-        }
-        return values;
+        return finalValues;
+      }
+      return values;
     }, '');
     res.setHeader('Content-Type', 'text/html');
     res.setHeader('Content-Length', body.length);
-    res.end(body);  
+    res.end(body);
   });
 }];
 
-exports.get =  [
-  passport.authenticate(['basic', 'oauth2-client-password', 'header'], { session: false }),
-  function(req, res){
-  if (req.param('queue') in states){
+exports.get = [
+passport.authenticate(['basic', 'oauth2-client-password', 'header'], {
+  session: false
+}),
+
+function(req, res) {
+  if (req.param('queue') in states) {
     console.log("status");
     status(req, res);
-  }
-  else{
+  } else {
     console.log("get");
     get(req, res);
-  } 
+  }
 }];
 
-exports.progress =  [
-  passport.authenticate(['basic', 'oauth2-client-password', 'header'], { session: false }),
-  function(req, res){
+exports.progress = [
+passport.authenticate(['basic', 'oauth2-client-password', 'header'], {
+  session: false
+}),
+
+function(req, res) {
   var queueId = req.param('queue');
   //total
-  models.EmailQueue.findOne({"rawrequest":queueId}).exec(function (err,data){    
-  if (data) {
-  models.EmailJob.count({"queue":data._id},function (err,dataTotal){    
-    logger("queueId:"+queueId);
-    logger("dataTotal:"+dataTotal);
-    logger("err:"+err);
-    logger("data._id:"+data._id);
-    if (dataTotal) {
-      //sent total
-      models.EmailJob.count({"queue":data._id, "status":"sent" },function (err,dataSent){          
-	var result = {progress: (dataSent/dataTotal)};
-	logger("result:"+result);
-        return res.json(200, result);
+  models.EmailQueue.findOne({
+    "rawrequest": queueId
+  }).exec(function(err, data) {
+    if (data) {
+      models.EmailJob.count({
+        "queue": data._id
+      }, function(err, dataTotal) {
+        logger("queueId:" + queueId);
+        logger("dataTotal:" + dataTotal);
+        logger("err:" + err);
+        logger("data._id:" + data._id);
+        if (dataTotal) {
+          //sent total
+          models.EmailJob.count({
+            "queue": data._id,
+            "status": "sent"
+          }, function(err, dataSent) {
+            var result = {
+              progress: (dataSent / dataTotal)
+            };
+            logger("result:" + result);
+            return res.json(200, result);
+          });
+        } else {
+          return res.json(500, {});
+        }
       });
+    } else {
+      res.json(404, {});
     }
-    else{
-      return res.json(500, {});
-    }
-  });
-  }else{
-	res.json(404,{});
-}
   });
 
 }];
 
-//this is not right need to rewrite because it is should be loading status it should be raw request
-function status(req, res){
-  var queues = models.EmailQueue.find({'status':req.param('queue')}).select().exec(function(err, data){
-    var body =  jsonJobFormatter(data);
-    res.setHeader('Content-Type', 'text/html');
-    res.setHeader('Content-Length', body.length);
-    res.end(body);  
+exports.report = [
+passport.authenticate(['basic', 'oauth2-client-password', 'header'], {
+  session: false
+}),
+
+function(req, res) {
+  var queueId = req.param('queue');
+  //queueId is rawrequest
+  generateSentFailedReport(queueId, res);
+}];
+
+
+function generateSentFailedReport(queueId, res) {
+  logger("result:" + queueId);
+  models.EmailQueue.findOne({
+    "rawrequest": queueId
+  }).exec(function(err, data) {
+    if (data) {
+      models.EmailJob.count({
+        "queue": data._id
+      }, function(err, dataTotal) {
+        logger("queueId:" + queueId);
+        logger("dataTotal:" + dataTotal);
+        logger("err:" + err);
+        logger("data._id:" + data._id);
+        if (dataTotal) {
+          //sent total
+          models.EmailJob.count({
+            "queue": data._id,
+            "status": "sent"
+          }, function(err, dataSent) {
+            var result = {
+              sent: dataSent,
+              notSent: (dataTotal - dataSent), //calculated
+              total: dataTotal
+            };
+            logger("result:" + result);
+            return res.json(200, result);
+          });
+        } else {
+          return res.json(500, {});
+        }
+      });
+    } else {
+      res.json(404, {});
+    }
   });
 }
 
-function get(req, res){
+//this is not right need to rewrite because it is should be loading status it should be raw request
+
+function status(req, res) {
+  var queues = models.EmailQueue.find({
+    'status': req.param('queue')
+  }).select().exec(function(err, data) {
+    var body = jsonJobFormatter(data);
+    res.setHeader('Content-Type', 'text/html');
+    res.setHeader('Content-Length', body.length);
+    res.end(body);
+  });
+}
+
+function get(req, res) {
   console.log("doing a get");
-  var queues = models.EmailQueue.findOne({'_id':req.param('queue')}).select().exec(function(err, data){
+  var queues = models.EmailQueue.findOne({
+    '_id': req.param('queue')
+  }).select().exec(function(err, data) {
     console.log("doing a get1");
-    var body =  jsonJobFormatter(data);
+    var body = jsonJobFormatter(data);
     console.log("doing a get2");
     res.setHeader('Content-Type', 'text/html');
     res.setHeader('Content-Length', body.length);
-    res.end(body);  
+    res.end(body);
   });
 }
 
-function jsonJobFormatter(data){
-  return JSON.stringify(data,function censor(key, values) {
-        if (key == "jobs") {
-          var finalValues = [];
-          console.log(values);
-          _.each(values, function(value, key, list){
-          value = '<a href=\'/job/'+value+'\'>'+value+'</a>';
-          finalValues.push(value);
-        });
-          return finalValues;
-        }
-        return values;
-    }, 4);
+function jsonJobFormatter(data) {
+  return JSON.stringify(data, function censor(key, values) {
+    if (key == "jobs") {
+      var finalValues = [];
+      console.log(values);
+      _.each(values, function(value, key, list) {
+        value = '<a href=\'/job/' + value + '\'>' + value + '</a>';
+        finalValues.push(value);
+      });
+      return finalValues;
+    }
+    return values;
+  }, 4);
 }
-
--- soho_server.js
+++ soho_server.js
@@ -1,13 +1,13 @@
 var requireDir = require('require-dir'),
     express = require('express'),
-	app = express(),
+    app = express(),
     _ = require("underscore"),
     mail = require('./mailer'),
     models = require('./models'),
     routes = requireDir('./routes'),
     crud = require('crud'),
     dust = require('dustjs-linkedin'),
-	cons = require('consolidate'),
+    cons = require('consolidate'),
     MongoStore = require('connect-mongo')(express),
     passport = require('passport'),
     auth = require("./auth"),
@@ -15,33 +15,35 @@ var requireDir = require('require-dir'),
 
 require("mongoose").connect('localhost', 'soho_mail');
 
-if ( !require('fs').existsSync(__dirname+"/raw/")) {
-    require('fs').mkdirSync(__dirname+"/raw/");    
+if (!require('fs').existsSync(__dirname + "/raw/")) {
+    require('fs').mkdirSync(__dirname + "/raw/");
 }
 
 
 // assign the dust engine to .dust files
 app.engine('dust', cons.dust);
-app.configure(function(){
+app.configure(function() {
     app.set('port', process.env.SOHOMAILPORT || 3001);
     app.set('view engine', 'dust');
     app.set('views', __dirname + '/views');
     app.set('root', __dirname);
-    app.use(express.static(__dirname + '/public', {redirect: false}));
+    app.use(express.static(__dirname + '/public', {
+        redirect: false
+    }));
     app.use(passport.initialize());
     app.use(routes.email.post);
     app.use(express.cookieParser());
     app.use(express.session({
-    	secret: 'foo',
-    	store: new MongoStore({
-      		db: 'soho_mail'
-    	})
-  	}));
+        secret: 'foo',
+        store: new MongoStore({
+            db: 'soho_mail'
+        })
+    }));
     app.use(express.bodyParser());
     app.use(passport.session());
     app.use(require('flashify'));
     app.use(app.router);
- });
+});
 
 dust.makeBase({
     copy: '&copy; 2011 Nobody LLC'
@@ -54,21 +56,22 @@ app.get('/', routes.home.get);
 //CRUD queue
 app.get('/queues', routes.queue.list);
 app.get('/queues/:queue/progress', routes.queue.progress);
+app.get('/queues/:queue/report', routes.queue.report);
 app.get('/queue/:queue', routes.queue.get);
 
 //CRUD jobs
-app.get('/jobs',routes.job.list);
-app.get('/jobs/:id',routes.job.get);
+app.get('/jobs', routes.job.list);
+app.get('/jobs/:id', routes.job.get);
 app.post('/jobs', routes.email.post);
 
-app.get('/templates',routes.template.list);
-app.get('/templates/:id',routes.template.get);
-app.post('/templates/:id',routes.template.update);
-app.post('/templates',routes.template.post);
+app.get('/templates', routes.template.list);
+app.get('/templates/:id', routes.template.get);
+app.post('/templates/:id', routes.template.update);
+app.post('/templates', routes.template.post);
 
 
-app.get('/demo/template',routes.demo.getTemplate);
-app.get('/demo/email',routes.demo.getEmail);
+app.get('/demo/template', routes.demo.getTemplate);
+app.get('/demo/email', routes.demo.getEmail);
 
 app.get('/users/new', routes.user.signupForm);
 app.get('/signup', routes.user.signupForm);
@@ -84,5 +87,5 @@ app.get('/oauth/auth', oauthserver.authorization);
 app.post('/oauth/auth', oauthserver.decision);
 app.post('/oauth/token');
 
-console.log("Listening Port = "+app.get('port'));
+console.log("Listening Port = " + app.get('port'));
 app.listen(app.get('port'));
